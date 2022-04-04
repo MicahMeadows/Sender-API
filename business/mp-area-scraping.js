f55ff44a9911e595d582c4a2a02a7aa32543823b
micahmeadows <micahstlp@gmail.com>
2022-04-03 23:44:36 -0400

    Update dockerfile to install chromium dependencies

--- business/mp-area-scraping.js
+++ business/mp-area-scraping.js
@@ -11,51 +11,65 @@ async function getSubAreas(parentAreaId = 0) {
 
     const subAreasRequestUrl = `https://www.mountainproject.com/ajax/public/area-picker-list/${parentAreaId}?mode=single&routes=0#`;
 
-    let browser = await puppeteer.launch({headless: true});
+    try {
 
-    let page = await browser.newPage();
-
-    await page.goto(subAreasRequestUrl, { waitUntil: 'networkidle2' });
-
-    let selectedClassName = await page.$eval('div > strong', (area) => {
-        return area.parentElement.className;
-    });
-
-    let parentLevel = parseLevelFromClass(selectedClassName);
-    console.log(`parent area level: ${parentLevel}`);
-
-
-    let areaData = await page.$$eval('div > a', (links) => {
-        let areas = [];
-        links.forEach((link) => {
-            let areaId = link.getAttribute('data-area-id');
-            let areaTitle = link.getAttribute('data-area-title');
-            let areaLevel = parseInt(link.parentElement.className.replace('l-', ''));
-
-            let areaInfo = {
-                "areaId": areaId,
-                "areaTitle": areaTitle,
-                "areaLevel": areaLevel
-            };
-            areas.push(areaInfo);
+        let browser = await puppeteer.launch({
+            headless: true,
+            args: [
+                "--disable-gpu",
+                "--no-sandbox",
+            ]
         });
+
+        let page = await browser.newPage();
+
+        await page.goto(subAreasRequestUrl, { waitUntil: 'networkidle2' });
+
+        let selectedClassName = await page.$eval('div > strong', (area) => {
+            return area.parentElement.className;
+        });
+
+        let parentLevel = parseLevelFromClass(selectedClassName);
+        console.log(`parent area level: ${parentLevel}`);
+
+        console.log('1. didnt break yet xD');
+
+        let areaData = await page.$$eval('div > a', (links) => {
+            let areas = [];
+            links.forEach((link) => {
+                let areaId = link.getAttribute('data-area-id');
+                let areaTitle = link.getAttribute('data-area-title');
+                let areaLevel = parseInt(link.parentElement.className.replace('l-', ''));
+
+                let areaInfo = {
+                    "areaId": areaId,
+                    "areaTitle": areaTitle,
+                    "areaLevel": areaLevel
+                };
+                areas.push(areaInfo);
+            });
         
-        return areas;
-    });
+            return areas;
+        });
 
-    const numResults = areaData.length;
+        console.log('2. didnt break yet xD');
 
-    const lastArea = areaData[numResults-1];
-    console.log(`last area level: ${lastArea.areaLevel}`);
+        const numResults = areaData.length;
 
-    await browser.close();
+        const lastArea = areaData[numResults-1];
+        console.log(`last area level: ${lastArea.areaLevel}`);
 
-    if (parentLevel == lastArea.areaLevel) {
-        console.log('is leaf node');
-        return [];
-    } else {
-        console.log(areaData);
-        return areaData;
+        await browser.close();
+
+        if (parentLevel == lastArea.areaLevel) {
+            console.log('is leaf node');
+            return [];
+        } else {
+            console.log(areaData);
+            return areaData;
+        }
+    } catch (e) {
+        console.log(e);
     }
 };
 
