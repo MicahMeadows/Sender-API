b84362f5e4f5f634dcb84c30298ff317653b5159
MicahMeadows <micahm213@gmail.com>
2022-09-07 15:17:33 -0400

    export app for cloud function

--- app.js
+++ app.js
@@ -62,6 +62,10 @@ app.use('/routes', exposeFirebaseServices, climbingRouteRoutes);
 app.use('/areas', exposeFirebaseServices, areaRoutes);
 app.use('/user', exposeFirebaseServices, checkAuth, userRoutes);
 
-app.listen(
-    port, () => console.log(`were live at http://localhost:${port}`)
-);
+// app.listen(
+//     port, () => console.log(`were live at http://localhost:${port}`)
+// );
+
+module.exports = {
+    app: app
+}
